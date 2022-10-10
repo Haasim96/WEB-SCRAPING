5552cb58f2973d0112ff9238068e1b2cead0fda0
Haasim96 <112970458+Haasim96@users.noreply.github.com>
2022-10-10 21:59:20 +0530

    Add files via upload

---
+++ web scrabing topbillioners.R
@@ -0,0 +1,37 @@
+# INSTALL LIBRARIES
+
+library(robotstxt)
+library(rvest)
+
+# CHECK WHETHER THE WEBSITE IS ALLOWED TO SCRAP
+
+Path= paths_allowed("https://en.wikipedia.org/wiki/The_World%27s_Billionaires")
+
+# ASSIGN THE WEBSITE TO A VARIABLE
+
+url<-"https://en.wikipedia.org/wiki/The_World%27s_Billionaires"
+
+# READ THE HTML PART OF THE URL
+my_html<-read_html(url)
+
+# SEGREGATE THE HTML NODES, GET THE TABLES AND ASSIGN THEM TO A VARIABLE
+
+my_tables <- html_nodes(my_html,"table")
+
+# TAKE THE FIRST TABLE AND ASSIGN THEM TO A SEPARATE VARIABLE
+billinores_table <- html_table(my_tables)[[1]]
+
+# VIEW THE TABLE
+View(billinores_table)
+
+# TAKE ONLY THE SECOND AND THIRD COLUMN FROM THE TABLE AND STORE IT IN A VARIABLE
+
+billinores <- billinores_table[,2:3]
+
+# VIEW THE TABLE
+View(billinores)
+
+# CHECK FOR DATATYPES
+
+str(billinores)
+
